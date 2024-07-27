398abe76d5bc012bfe4369621a7a22eadb4d56c4
abiasv <vaziri.as@gmail.com>
2024-07-26 21:06:27 -0400

    first commit

---
+++ backend/resolvers/user.resolver.js
@@ -0,0 +1,14 @@
+import { users } from "../dummyData/data.js";
+const userResolver = {
+	Query: {
+		users: (_, { req, res }) => {
+			return users;
+		},
+		user: (_, { userId }) => {
+			return users.find((user) => user._id === userId);
+		},
+	},
+	Mutation: {},
+};
+
+export default userResolver;
